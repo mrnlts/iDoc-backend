ff5dc4b4a1c86318369060409a16a22d040736a8
marina <marinaaltesmagret@gmail.com>
2021-07-31 18:23:12 +0200

    Prepares config for deployment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
@@ -25,7 +25,7 @@ async function setupApp() {
 	app.use(logger('dev'));
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: false }));
-	app.use(cookieParser());
+	// app.use(cookieParser());
 	app.use(express.static(path.join(__dirname, 'public')));
 	app.use(
 		session({
@@ -39,6 +39,7 @@ async function setupApp() {
 			cookie: {
 				maxAge: 24 * 60 * 60 * 1000,
 				sameSite: 'none',
+				secure: true,
 			},
 		})
 	);
